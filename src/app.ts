f216790f94731c9280b60cda2af8138e9b7d0ded
sscarry2 <earth.narongsak@gmail.com>
2021-12-12 21:59:03 +0700

    setup project

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import config from 'config'
+
+// import connect from './utils/connect'
+// import logger from './utils/logger'
+// import routes from './routes'
+// import deserializeUser from './middleware/deserializeUser'
+
+// const port = config.get<string>('port')
+const port = 8080
+
+
+const app = express()
+
+app.use(express.json())
+
+// app.use(deserializeUser)
+
+app.get('/', (req, res) => {
+    return res.send('Hello World')
+})
+
+app.listen(port, async () => {
+    // logger.info(`Listening on http://localhost:${port}`)
+    // await connect()
+
+    // routes(app)
+
+    console.log(`Connected on port: ${port}`);
+    
+
+
+})
