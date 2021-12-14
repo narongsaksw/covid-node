c84c824fd633fa6a695245d5adda9e2c84fe5776
sscarry2 <earth.narongsak@gmail.com>
2021-12-15 00:40:41 +0700

    add get method /api/users

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Express, Request, Response } from 'express'
 
-import { createUserHandler } from './controllers/user.controller'
+import { createUserHandler, getAllUsersHandler } from './controllers/user.controller'
 
 
 function routes(app: Express) {
     app.post('/api/users',createUserHandler)
+    app.get('/api/users',getAllUsersHandler)
 }
 
 export default routes
