7c5127765c6b4e944c67e4679448a2ca4e77fe68
sscarry2 <earth.narongsak@gmail.com>
2021-12-17 16:39:19 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 import config from 'config'
+import cors from 'cors'
 
 import connect from './utils/connect'
 import routes from './routes'
 
-// const port = config.get<string>('port')
-const port = 8080
+const port = config.get<string>('port')
 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.get('/', (req, res) => {
     return res.send('Hello World')
